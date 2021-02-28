cbed09d8663a67a9600b59a4c66a91491e747911
Zero <imadma42@gmail.com>
2021-02-28 18:01:30 +0300

    v0.9 pre release
    v1.0 coming soon

---
+++ javascript.js
@@ -0,0 +1,29 @@
+window.onload = function(){
+  //variables
+  var form = document.getElementById("form");
+  var input = document.getElementById('input');
+  var btn = document.getElementById('btn');
+  var list = document .getElementById('list');
+  var id = 1;
+
+   //button event listener
+   btn.addEventListener("click", addTodoItem);
+   form.addEventListener("submit", addTodoItem)
+   // add todo item to list
+   function addTodoItem(){
+     if(input.value === ""){
+       alert("you must enter some value")
+     }
+     else{
+      if(list.style.borderTop === ""){
+        list.style.borderTop = "2px solid white";
+      }
+      var text = input.value;
+      var item = `<li id="li-${id}">${text}
+                  <input id="box-${id}" class="checkboxes" type="checkbox"></li>`;
+                  list.insertAdjacentHTML('beforeend',item);
+                  id++;
+                  form.reset();
+     }
+   }
+}
--- script.js
+++
@@ -1,9 +0,0 @@
-//where is my problem 
-function add() {
-var text = prompt();
-  var h5 = document.createElement("h5");
-  var main = document.getElementsByTagName('main');
-  var div = document.createElement("div")
-  main.appendChild(div);
-    div.appendChild(h5);
-      h5.appendChild(document.createTextNode(text))}
